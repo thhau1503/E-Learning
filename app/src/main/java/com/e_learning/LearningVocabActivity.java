17be65834a3729e5944ae377c3ffcaa65d2b4bf7
thhau1503 <thanhhaudeb204@gmail.com>
2024-04-18 16:09:52 +0700

    update 18/4/2024

---
+++ app/src/main/java/com/e_learning/LearningVocabActivity.java
@@ -0,0 +1,14 @@
+package com.e_learning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearningVocabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_vocab);
+    }
+}
